f739cd6f2a0a458806732861d9893867b56c5a34
Frank Li <44486240+GamingFrankie@users.noreply.github.com>
2020-04-07 16:16:18 -0400

    Update Cylinder.py

--- Wave 1/Cylinder.py
+++ Wave 1/Cylinder.py
@@ -1,1 +1,18 @@
 
+import math
+print("Welcome to the Cylinder Volume Calculator!")
+print("-----------------------------------")
+
+while True:
+  height = float(input("Please input the height of the CYLINDER: "))
+  rad = float(input("Please input the base radius the CYLINDER: "))
+
+  print("-----------------------------------")
+  print("The HEIGHT is", height)
+  print("The RADIUS is", rad)
+
+  print("-----------------------------------")
+  print("Confirm?")
+  print("Yes = Y")
+  print("No = N")
+  answer = input("Your Answer: ")
