366b2fcb43913f85460898b3580c5d912c7fea57
Frank Li <44486240+GamingFrankie@users.noreply.github.com>
2020-04-07 16:23:59 -0400

    Update Cylinder-Simplified.py

--- Wave 1/Cylinder-Simplified.py
+++ Wave 1/Cylinder-Simplified.py
@@ -1,1 +1,12 @@
 
+import math
+print("Welcome to the Cylinder Volume Calculator!")
+print("-----------------------------------")
+
+height = float(input("Please input the height of the CYLINDER: "))
+rad = float(input("Please input the base radius the CYLINDER: "))
+print("-----------------------------------")
+
+circle = rad * rad * math.pi
+area = circle * height  
+print("The Volume of the CYLINDER is:", round(area, 1))
