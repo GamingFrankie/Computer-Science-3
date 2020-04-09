7a4853bb7b91c2c02bf6d826758ccce8f7c693f0
Frank Li <44486240+GamingFrankie@users.noreply.github.com>
2020-04-09 17:24:08 -0400

    Update and rename Compound - Simplyfied.py to Compound-Simplyfied.py

--- Wave 1/Compound-Simplyfied.py
+++ Wave 1/Compound-Simplyfied.py
@@ -1,20 +1,18 @@
-print("Welcome to Compound Interest Calculator!")
 
 percentage = float(input("What is the percentage of interest? "))
 interest = percentage * 0.01
-time = float(input("Number of time compounded in one year? "))
 save = float(input("How much is your saving? "))
 
 # A = P * (1 + r / n)^nt
 
 # Year 1
-yearOne = (1 + interest / time)
+yearOne = (1 + interest / 1)
 
 # Year 2
-yearTwo = (1 + interest / time) * (1 + interest / time)
+yearTwo = (1 + interest / 1) * (1 + interest / 1)
 
 # Year 3
-yearThree = (1 + interest / time) * (1 + interest / time) * (1 + interest / time)
+yearThree = (1 + interest / 1) * (1 + interest / 1) * (1 + interest / 1)
 
 print("The amount after first year is: "'%.2f' % round(save * yearOne, 2))
 print("The amount after second year is: "'%.2f' % round(save * yearTwo, 2))
