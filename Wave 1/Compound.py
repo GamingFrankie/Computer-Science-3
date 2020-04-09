6b71bfc7d1fe2009233dfd274021f418441fe066
Frank Li <44486240+GamingFrankie@users.noreply.github.com>
2020-04-09 17:01:59 -0400

    Create Compound.py

---
+++ Wave 1/Compound.py
@@ -0,0 +1,5 @@
+print("Welcome to Compound Interest Calculator!")
+
+interest = float(input("What is the percentage of interest? "))
+time = float(input("Number of time compounded in one year? "))
+save = float(input("How much is your saving? "))
