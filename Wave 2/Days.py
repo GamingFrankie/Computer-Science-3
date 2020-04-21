848e3b2f004323fa8742e9890aeb443d85f01ac9
Tianyu Li <44486240+GamingFrankie@users.noreply.github.com>
2020-04-21 10:02:52 -0400

    Create Days.py

---
+++ Wave 2/Days.py
@@ -0,0 +1,24 @@
+while True:
+  print("What is the name of the month?")
+  date = input("Your answer (Eg: January/Jan): ")
+  print("-----------------------------------")
+  if date == "February" or date == "Feb":
+    print("The number of days in this month is 28 or 29.")
+  elif date == "January" or date == "Jan" or date == "March" or date == "Mar" or date == "May" or date == "July" or date == "Jul" or date == "August" or date == "Aug" or date == "October" or date == "Oct" or date == "December" or date == "Dec":
+    print("The number of days in this month is 31.")
+  elif date == "April" or date == "Apr" or date == "June" or date == "Jun" or date == "September" or date == "Sep" or date == "November" or date == "Nov":
+    print("The number of days in this month is 30.")
+  else:
+    print("Syntax Error, Please Check The Spelling And The Initial.")
+  print("-----------------------------------")
+  print("Exit?")
+  print("Yes = Y")
+  print("No = N")
+  answer = input("Your Answer: ")
+  if answer == "Y":
+      break
+  elif answer == "N":
+      print("-----------------------------------")
+  else:
+      print("Invalid answer.")
+      print("-----------------------------------")
