21cf4f391096824454d9068f1f89384b9f6b2ce0
Tianyu Li <44486240+GamingFrankie@users.noreply.github.com>
2020-04-24 12:06:37 -0400

    Create Quadratic-Simplified.py

---
+++ Wave 2/Quadratic-Simplified.py
@@ -0,0 +1,7 @@
+# Number of roots: b * b - 4ac
+# If ans > 0, number of roots = 2
+# If ans = 0, number of root = 1
+# If ans < 0, number of root = 0
+
+# Quadratic Formula:
+# (-b +- (b * b - 4 * a * c) / (b * b - 4 * a * c)) / 2 * a
