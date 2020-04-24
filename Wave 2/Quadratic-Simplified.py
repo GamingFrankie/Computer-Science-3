db3482908595176ce30364bcc48c18796b361cd5
Tianyu Li <44486240+GamingFrankie@users.noreply.github.com>
2020-04-24 12:50:57 -0400

    Update Quadratic-Simplified.py

--- Wave 2/Quadratic-Simplified.py
+++ Wave 2/Quadratic-Simplified.py
@@ -4,4 +4,26 @@
 # If ans < 0, number of root = 0
 
 # Quadratic Formula:
-# (-b +- (b * b - 4 * a * c) / (b * b - 4 * a * c)) / 2 * a
+# ax**2 + bx + c = 0
+import cmath
+a = float(input("What is the 'a' value? "))
+b = float(input("What is the 'b' value? "))
+c = float(input("What is the 'c' value? "))
+
+if (b ** 2 - 4 * a * c) > 0:
+  print("There are two roots.")
+  root = 2
+elif (b ** 2 - 4 * a * c) == 0:
+  print("There is one root.")
+  root = 1
+elif (b ** 2 - 4 * a * c) < 0:
+  print("There is no root.")
+  root = 0
+if root == 2 or root == 1:
+  solOne = (-b - cmath.sqrt((b ** 2 - 4 * a * c))) / (2 * a)
+  solTwo = (-b + cmath.sqrt((b ** 2 - 4 * a * c))) / (2 * a) 
+
+  if solOne == solTwo:
+    print(solOne)
+  else:
+    print(solOne, "and", solTwo)
